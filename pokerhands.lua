024da3302983d9fdb720dc4eeaa709313e9c202c
moothy-14 <moothzy@gmail.com>
2025-10-01 00:01:41 -0700

    Modified Pokerhand chips to 0, and set display order

---
+++ pokerhands.lua
@@ -0,0 +1,100 @@
+--- TODO:
+--- Make royal flush with 10 mult (Order offset 9)
+
+SMODS.PokerHand:take_ownership("High Card",
+    {
+    chips = 0,
+    order_offset = 0,
+    },
+
+    true
+)
+
+SMODS.PokerHand:take_ownership("Pair",
+    {
+    chips = 0,
+    order_offset = 1,
+    },
+    true
+)
+
+SMODS.PokerHand:take_ownership("Two Pair",
+    {
+    chips = 0,
+    order_offset = 2,
+    },
+    true
+)
+
+SMODS.PokerHand:take_ownership("Three of a Kind",
+    {
+    chips = 0,
+    order_offset = 3,
+    },
+    true
+)
+
+SMODS.PokerHand:take_ownership("Straight",
+    {
+    chips = 0,
+    order_offset = 4,
+    },
+    true
+)
+
+SMODS.PokerHand:take_ownership("Flush",
+    {
+    chips = 0,
+    order_offset = 5,
+    },
+    true
+)
+
+SMODS.PokerHand:take_ownership("Full House",
+    {
+    chips = 0,
+    order_offset = 6,
+    },
+    true
+)
+
+SMODS.PokerHand:take_ownership("Four of a Kind",
+    {
+    chips = 0,
+    order_offset = 7,
+    },
+    true
+)
+
+SMODS.PokerHand:take_ownership("Straight Flush",
+    {
+    chips = 0,
+    order_offset = 8,
+    },
+    true
+)
+
+SMODS.PokerHand:take_ownership("Five of a Kind",
+    {
+    chips = 0,
+    order_offset = 10,
+    },
+    true
+)
+
+SMODS.PokerHand:take_ownership("Flush House",
+    {
+    chips = 0,
+    order_offset = 11,
+    },
+    true
+)
+
+SMODS.PokerHand:take_ownership("Flush Five",
+    {
+    chips = 0,
+    order_offset = 12,
+    },
+    true
+)
+
